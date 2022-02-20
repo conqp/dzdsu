e9a31195c1dd5449fa0a51efb9bc9809a2aa2abf
Richard Neumann <mail@richard-neumann.de>
2022-02-20 17:44:37 +0100

    Remove ModMetadata from __init__.py

--- dzdsu/__init__.py
+++ dzdsu/__init__.py
@@ -1,7 +1,7 @@
 """DayZ dedicated server utilities."""
 
 from dzdsu.constants import JSON_FILE
-from dzdsu.mods import Mod, ModMetadata, InstalledMod, mods_str, print_mods
+from dzdsu.mods import Mod, InstalledMod, mods_str, print_mods
 from dzdsu.params import ServerParams
 from dzdsu.server import Server, load_servers
 from dzdsu.update import Updater
@@ -11,7 +11,6 @@ __all__ = [
     'JSON_FILE',
     'InstalledMod',
     'Mod',
-    'ModMetadata',
     'Server',
     'ServerParams',
     'Updater',
