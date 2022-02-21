6515594886dccf44419302e9bbcbc867cbc4c530
Richard Neumann <r.neumann@homeinfo.de>
2022-02-21 13:44:18 +0100

    Implement wiping

--- dzdsu/mission.py
+++ dzdsu/mission.py
@@ -1,8 +1,8 @@
 """Mission management."""
 
 from pathlib import Path
+from shutil import rmtree
 from tarfile import TarFile
-from typing import Iterator
 
 
 __all__ = ['Mission']
@@ -15,12 +15,25 @@ class Mission:
         self.path = path
 
     @property
-    def files(self) -> Iterator[Path]:
-        """Yields top-level files."""
-        return self.path.iterdir()
+    def name(self) -> str:
+        """Returns the mission name."""
+        return self.path.name
+
+    @property
+    def storage_1(self) -> Path:
+        """Returns the path to the storage_1 folter."""
+        return self.path / 'storage_1'
 
     def backup(self, archive: Path):
         """Creates a backup of the mission."""
         with TarFile.open(archive, mode='w:gz') as tarfile:
-            for file in self.files:
-                tarfile.add(file)
+            for file_or_dir in self.path.iterdir():
+                tarfile.add(file_or_dir)
+
+    def wipe(self) -> None:
+        """Wipes the mission data."""
+        for file_or_dir in self.storage_1.iterdir():
+            if file_or_dir.is_dir():
+                rmtree(file_or_dir)
+            else:
+                file_or_dir.unlink()
