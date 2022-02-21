952b9dfc47a0ce81911d509a65155882dbd4110d
Richard Neumann <r.neumann@homeinfo.de>
2022-02-21 14:11:29 +0100

    Incorporate server name into backup file name

--- dzdsu/utility/backup.py
+++ dzdsu/utility/backup.py
@@ -10,20 +10,20 @@ from dzdsu.utility.logger import LOGGER
 __all__ = ['backup']
 
 
-def gen_filename(mission: str) -> str:
+def gen_filename(server: Server, mission: str) -> str:
     """Generates a file name."""
 
-    return f'{mission}-{datetime.now().isoformat()}.tar.gz'
+    return f'{server.name}-{mission}-{datetime.now().isoformat()}.tar.gz'
 
 
 def backup(server: Server, args: Namespace) -> int:
     """Creates a backup of the server."""
 
     for mission in set(args.mission):
-        if (path := args.backup_dir / gen_filename(mission)).exists():
-            LOGGER.error('Backup file "%s" already exists.', path)
+        if (file := args.backup_dir / gen_filename(server, mission)).exists():
+            LOGGER.error('Backup file "%s" already exists.', file)
             return 1
 
-        server.mission(mission).backup(args.backup_dir / gen_filename(mission))
+        server.mission(mission).backup(file)
 
     return 0
